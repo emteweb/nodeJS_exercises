6641f8630e522702925241993ec00b3f611bb114
Marcin Trebacz <emteweb@gmail.com>
2020-09-27 12:18:20 +0200

    variables and loops in ejs files

--- DemoApp/HTML_Templates_and_EJS/app.js
+++ DemoApp/HTML_Templates_and_EJS/app.js
@@ -10,4 +10,14 @@ app.get("/youareon/:subpage", function(req,res){
     res.render("subpage.ejs", {subPageVar: subpage}); // in render we pass an JS Object which is executed in .ejs file 
 })
 
+app.get("/posts", function(req,res){
+    var posts = [
+        {title: "Post1", author: "John"},
+        {title: "Post2", author: "Mary"},
+        {title: "Post3", author: "Michael"},
+    ];
+
+    res.render("posts.ejs", {posts: posts});
+})
+
 app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
