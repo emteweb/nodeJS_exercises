3c7672d71fb12f3597aa7b375c40a19880ae6a46
Marcin Trebacz <emteweb@gmail.com>
2020-09-30 21:17:52 +0200

    Post Request Demo and API calls

---
+++ DemoApp/APIs/first_request.js
@@ -0,0 +1,47 @@
+// request package is now deprecated
+//var request = require("request");
+
+/* request("https://jsonplaceholder.typicode.com/todos", function(error, response, body){
+    if(!error && response.statusCode == 200){
+        console.log(body);
+    }
+}); */
+
+const axios = require('axios');
+
+// Make a request for a user with a given ID
+axios.get('https://jsonplaceholder.typicode.com/todos/1')
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+    console.log(response.data.title);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
+  // 2nd possibility:
+
+  // A) with EXPRESS:
+  /* app.get('/', async (req,res,next) => {
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+      } catch(err){
+          console.log(err);
+      }
+  }); */
+  
+  // B) in the console
+
+  (async () => {
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+        console.log(response.data);
+      } catch(err){
+          console.log(err);
+      }
+  })();
