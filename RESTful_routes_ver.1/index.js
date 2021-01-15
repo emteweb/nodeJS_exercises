273bdb8da5f61529f948a25d00edfec7f7220bee
Marcin T <fake_mail@gmail.com>
2021-01-15 19:32:39 +0100

    add new comments

--- RESTful_routes_ver.1/index.js
+++ RESTful_routes_ver.1/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const {v4: uuidv4} = require('uuid'); // for generating unique IDs
+uuidv4();
 
 app.use(express.urlencoded({ extended: true })) // for parsing data we send via POST request
 app.use(express.json()) // built-in middleware function for parsing application/json
@@ -9,18 +11,22 @@ app.set("view engine", "ejs");
 
 const comments = [
     {
+        id: uuidv4(),
         username: 'Todd',
         comment: 'This is the first comment'
     },
     {
+        id: uuidv4(),
         username: 'Adam',
         comment: 'This is the second comment'
     },
     {
+        id: uuidv4(),
         username: 'Alison',
         comment: 'This is the third comment'
     },
     {
+        id: uuidv4(),
         username: 'David',
         comment: 'This is the fourth comment'
     }
@@ -30,7 +36,19 @@ const comments = [
 app.get('/comments', (req,res)=>{
     res.render('comments/index', {comments});
 })
-
+app.get('/comments/new', (req,res)=>{
+    res.render('comments/new');
+})
+app.post('/comments/', (req,res)=>{
+    const {username, comment} = req.body;
+    comments.push({username, comment, id: uuidv4()});
+    res.redirect("/comments");
+})
+app.get('/comments/:id', (req, res)=>{
+    const {id} = req.params;
+    const comment = comments.find(c => c.id === id); // we use parseInt because from params we get a string
+    res.render('comments/show', {comment});
+})
 
 
 
