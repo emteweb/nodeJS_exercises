5d9fe07fe4f126b480485e7bc7cbe5b4d81bdc71
Marcin T <fake_mail@gmail.com>
2021-01-16 18:22:15 +0100

    add CRUD to comments

--- RESTful_routes_ver.1/index.js
+++ RESTful_routes_ver.1/index.js
@@ -3,13 +3,15 @@ const app = express();
 const path = require('path');
 const {v4: uuidv4} = require('uuid'); // for generating unique IDs
 uuidv4();
+const methodOverride = require('method-override'); // to make other types of request possible from the browser
 
 app.use(express.urlencoded({ extended: true })) // for parsing data we send via POST request
 app.use(express.json()) // built-in middleware function for parsing application/json
+app.use(methodOverride('_method'));
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 
-const comments = [
+let comments = [
     {
         id: uuidv4(),
         username: 'Todd',
@@ -49,19 +51,25 @@ app.get('/comments/:id', (req, res)=>{
     const comment = comments.find(c => c.id === id); // we use parseInt because from params we get a string
     res.render('comments/show', {comment});
 })
+app.get('/comments/:id/edit', (req,res)=>{
+    const {id} = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', {comment})
+})
 
+app.patch('/comments/:id', (req,res)=>{
+    const {id} = req.params;
+    const newComment = req.body.comment;
+    const foundComment = comments.find(c => c.id === id);
+    foundComment.comment = newComment;
+    res.redirect('/comments');
+})
 
-
-
-
-
-
-
-
-
-
-
-
+app.delete('/comments/:id', (req,res)=>{
+    const {id} = req.params;
+    const comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments')    
+})
 
 app.listen(3000, ()=>{
     console.log('Listening on port 3000.')
